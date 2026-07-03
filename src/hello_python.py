5d2412c8de85ab31c07338f844a238921103edeb
Kaushalkumar Patel <kaushalkpatel@hotmail.com>
2026-07-03 19:08:35 +0530

    Complete Sprint 1 Day 4

---
+++ src/hello_python.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from datetime import datetime
+
+print(f"Name: Kaushal Patel")
+print(f"Python Version: {sys.version}")
+print(f"Current Date & Time: {datetime.now()}")
+print(f"Current Working Directory: {os.getcwd()}")
